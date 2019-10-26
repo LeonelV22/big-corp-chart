1a476d501fa65aa90a651cb990ff0b8afa22416c
Leonel Viera <leonelviera22@gmail.com>
2019-10-26 12:09:23 -0300

    Remove innecesary props

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Employee from './Employee';
 
-const EmployeesList = React.memo(({ employees = [], setEmployees }) => {
+const EmployeesList = React.memo(({ employees = [] }) => {
 
     if (employees.length === 0) {
-        return null;
+        return null
     }
     return (
         <React.Fragment>
-            
             <div className='EmployeeContainer'>
                 {
-                    employees.map(employee => <Employee key={employee.id} employee={employee} employees={employees} setEmployees={setEmployees} />)
+                    employees.map(employee => <Employee key={employee.id} employee={employee} />)
                 }
             </div>
         </React.Fragment>
